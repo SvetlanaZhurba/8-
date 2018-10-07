c43ab5c68fe1f4aac8cef196531302116ea497e1
SvetlanaZhurba <43250637+SvetlanaZhurba@users.noreply.github.com>
2018-10-07 19:52:05 +0300

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+// определение символических констант
+#define YES		1
+#define NO		0
+#define MAXLINE 1000
+
+void process_line(char buffer[]);
+
+int main(void)
+{
+	char line[MAXLINE];
+	fgets(line, MAXLINE, stdin);
+	process_line(line);
+	puts(line);
+	getchar();
+	return 0;
+}
+
+void process_line(char buffer[])
+{
+	char *buf_ptr; // указатель на текущий символ буфера
+	char *end; // указатель на конец слова
+	char *begin; // указатель на начало слова
+	char c; // текущий символ
+	char prev_c; // предыдущий символ
+	int flag; // признак слова
+	int found; // признак того, что буквы не повторились
+	int repeat; // признак того, что буквы повторились
+
+	flag = NO;
+	found = NO;
+	prev_c = ' ';
+	repeat = NO;
+
+	buf_ptr = buffer;
+	begin = buffer;
+	end = buffer;
+
+	do
+	{
+		c = *buf_ptr;
+
+		if ((c <'a' || c>'z') && (c<'A' || c>'Z'))
+		{
+			if (flag == YES)
+			{
+				// найден первый разделить после слова
+				end = buf_ptr - 1;
+				if ((found == NO) && (repeat == NO))
+				{
+					// две последних буквы в слове были одинаковы
+					// слово нужно удалить
+					char *src = end + 1;
+					char *dst = begin;
+					// удаление слова (перенос оставшейся части
+					// строки)
+					do
+					{
+						*dst = *src;
+						dst++;
+						src++;
+					} while (*src);
+					buf_ptr = begin; // начать обработку строки с
+									 // перенесённого фрагмента
+				}
+				else
+					if (repeat == YES)
+						repeat = NO;
+			}
+			flag = NO;
+		}
+		else
+		{
+			if (flag == NO)
+				begin = buf_ptr;
+
+
+			if (prev_c == c)
+			{
+
+				repeat = YES;
+
+			}
+			else
+				found = NO;
+
+
+			flag = YES;
+
+		}
+		prev_c = c;
+		buf_ptr++;
+
+	} while (c != '\0');
+
+}
